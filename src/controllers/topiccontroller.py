c4c07eb8417663bebf898050804ef4f39b2a124e
p0ntan <pontus.akerberg@hotmail.com>
2024-06-02 20:36:40 +0200

    Add controllers

---
+++ src/controllers/topiccontroller.py
@@ -0,0 +1,6 @@
+from src.controllers.controller import Controller
+from src.utils.dao import DAO
+
+class TopicController(Controller):
+  def __init__(self, topic_dao: DAO):
+    super().__init__(dao=topic_dao)
