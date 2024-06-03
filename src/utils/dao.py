23f68e99504b96f74809abe5c91d6327f2dfc101
p0ntan <pontus.akerberg@hotmail.com>
2024-06-03 09:21:39 +0200

    Fix pylint errors

--- src/utils/dao.py
+++ src/utils/dao.py
@@ -1,12 +1,18 @@
+"""
+DAO is used for simplyfying handling with data from database.
+"""
 import os
 import sqlite3
 import datetime
 
 
 class DAO:
-  def __init__(self, table_name: str):
-    """ DAO is for simpler access for tables in sqlite-db.
+  """
+  DAO is for simpler access for tables in sqlite-db.
+  """
 
+  def __init__(self, table_name: str):
+    """
     Parameters:
       table_name (str): tablename for the DAO.
     """
@@ -56,11 +62,11 @@ class DAO:
       self._disconnect()
       raise err
 
-  def get_one(self, id: int) -> dict | None:
+  def get_one(self, id_num: int) -> dict:
     """ Get one entry
 
     Parameters:
-      id (int):     the unique id for the entry in database
+      id_num (int): the unique id for the entry in database
 
     Returns:
       dict:         with data from single entry
@@ -72,7 +78,7 @@ class DAO:
     try:
       cur = self._connect_get_cursor()
 
-      cur.execute(f"SELECT * FROM {self._table} WHERE id = ?", (id, ))
+      cur.execute(f"SELECT * FROM {self._table} WHERE id = ?", (id_num, ))
       column_names = [description[0] for description in cur.description]
 
       result = cur.fetchone()
@@ -114,11 +120,11 @@ class DAO:
       self._disconnect()
       raise err
 
-  def update(self, id: int, data: dict) -> bool:
+  def update(self, id_num: int, data: dict) -> bool:
     """ Update entry.
 
     Parameters:
-      id (int):     unique id for the entry to update
+      id_num (int): unique id for the entry to update
       data (dict):  dictionary with new data
 
     Returns:
@@ -134,7 +140,7 @@ class DAO:
 
       columns = ', '.join([f'{k} = ?' for k in data.keys()])
 
-      cur.execute(f"UPDATE {self._table} SET {columns} WHERE id = ?", (*data.values(), id, ))
+      cur.execute(f"UPDATE {self._table} SET {columns} WHERE id = ?", (*data.values(), id_num, ))
       self._connection.commit()
       self._disconnect()
 
@@ -143,11 +149,11 @@ class DAO:
       self._disconnect()
       raise err
 
-  def delete(self, id: int) -> bool:
+  def delete(self, id_num: int) -> bool:
     """ Delete entry (soft delete).
 
     Parameters:
-      id (int):     unique id for the entry to delete
+      id_num (int): unique id for the entry to delete
 
     Returns:
       boolean:      True if item chaned, False otherwise
@@ -159,7 +165,7 @@ class DAO:
       now = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
       cur = self._connect_get_cursor()
 
-      cur.execute(f"UPDATE {self._table} SET DELETED = ? WHERE id = ?", (now, id, ))
+      cur.execute(f"UPDATE {self._table} SET DELETED = ? WHERE id = ?", (now, id_num, ))
       self._connection.commit()
       self._disconnect()
 
