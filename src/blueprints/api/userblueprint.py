cf918c03a6c36aa2ddd0fe52da9c75e88597aa49
p0ntan <pontus.akerberg@hotmail.com>
2024-06-03 12:01:03 +0200

    Pylint fixes

--- src/blueprints/api/userblueprint.py
+++ src/blueprints/api/userblueprint.py
@@ -1,8 +1,12 @@
+"""
+Blueprint for api route /user
+"""
 from flask import Blueprint, jsonify, abort, request
 from flask_cors import cross_origin
 from src.controllers.controller_repository import ControllerRepository
 
 user_blueprint = Blueprint('user_blueprint', __name__, url_prefix="/user")
+
 
 @user_blueprint.route("/all", methods=["GET"])
 def get_all():
@@ -16,24 +20,24 @@ def get_all():
     print(err)
     abort(500, "Unknown server error, try again.")
 
-@user_blueprint.route("/<id>", methods=["GET"])
-def get_one(id):
+@user_blueprint.route("/<id_num>", methods=["GET"])
+def get_one(id_num):
   """ Get info from one user. """
   try:
     uc_instance = ControllerRepository().get_user_controller()
-    data = uc_instance.get_one(id)
+    data = uc_instance.get_one(id_num)
     return jsonify(data)
   except Exception as err:
     print(err)
     abort(500, "Unknown server error, try again.")
 
-@user_blueprint.route("/<id>", methods=["PUT"])
-def put_one(id):
+@user_blueprint.route("/<id_num>", methods=["PUT"])
+def put_one(id_num):
   """ Update a user. """
   try:
     uc_instance = ControllerRepository().get_user_controller()
     data = request.json
-    result = uc_instance.update(id, data)
+    result = uc_instance.update(id_num, data)
     return jsonify({"success": result})
   except Exception as err:
     print(err)
