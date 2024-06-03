f2cca0c58d65e6fc3d1a4a704f1d5c69ae8f236e
p0ntan <pontus.akerberg@hotmail.com>
2024-06-03 11:42:23 +0200

    Update main file so application can run

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,10 +2,12 @@
 # Important to use application and not app for start with passenger
 
 from flask import Flask
+from dotenv import load_dotenv
 from src.blueprints.api.apiblueprint import api_blueprint
 
-application = Flask(__name__)
+load_dotenv()
 
+application = Flask(__name__)
 application.register_blueprint(api_blueprint)
 
 if __name__ == "__main__":
