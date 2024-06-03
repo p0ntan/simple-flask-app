f3593a19354118b6fbc1f0dc4b27c53ead5c699d
p0ntan <pontus.akerberg@hotmail.com>
2024-06-03 10:10:36 +0200

    Add apiblueprint file for strucutre

---
+++ src/blueprints/api/apiblueprint.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+from src.blueprints.api.userblueprint import user_blueprint
+
+api_blueprint = Blueprint('api_blueprint', __name__, url_prefix="/api")
+
+api_blueprint.register_blueprint(user_blueprint)
