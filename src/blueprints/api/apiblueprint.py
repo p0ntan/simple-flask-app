c56d44e1e676bf2321031cce0b44ba0683845e78
p0ntan <pontus.akerberg@hotmail.com>
2024-06-03 20:26:54 +0200

    Add post route

--- src/blueprints/api/apiblueprint.py
+++ src/blueprints/api/apiblueprint.py
@@ -3,6 +3,8 @@ API-blueprint, just a file for collecting all API-routes.
 """
 from flask import Blueprint
 from src.blueprints.api.userblueprint import user_blueprint
+from src.blueprints.api.postblueprint import post_blueprint
 
 api_blueprint = Blueprint('api_blueprint', __name__, url_prefix="/api")
 api_blueprint.register_blueprint(user_blueprint)
+api_blueprint.register_blueprint(post_blueprint)
