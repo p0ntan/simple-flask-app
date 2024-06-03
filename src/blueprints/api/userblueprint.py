43ea5e7e195d1e843102ad6cce89e513ff686030
p0ntan <pontus.akerberg@hotmail.com>
2024-06-03 20:10:14 +0200

    Refactor and minimize code

--- src/blueprints/api/userblueprint.py
+++ src/blueprints/api/userblueprint.py
@@ -12,53 +12,35 @@ user_blueprint = Blueprint('user_blueprint', __name__, url_prefix="/user")
 uc_instance = ControllerRepository().get_user_controller()
 
 
-@user_blueprint.route("/", methods=["GET"])
-def get_users():
-  # TODO Remove this ? When will this be used?
-  """ Get all users. """
+@user_blueprint.route("/", methods=["GET", "POST"])
+def index():
+  """ Get all users or create new. """
   try:
-    data = uc_instance.get_all()
-    response, status = r_helper.success_response(data)
+    # TODO Remove this ? When will this be used?
+    if request.method == "GET":
+      data = uc_instance.get_all()
+      response, status = r_helper.success_response(data)
+    elif request.method == "POST":
+      result = uc_instance.create(request.json)
+      response, status = r_helper.success_response(result, message="New user added.", status=201)
   except Exception as err:
     response, status = r_helper.unkown_error(details=f"{err}")
 
   return jsonify(response), status
 
 
-@user_blueprint.route("/<id_num>", methods=["GET"])
-def get_user(id_num):
+@user_blueprint.route("/<id_num>", methods=["GET", "PUT"])
+def single_user(id_num):
   """ Get info from one user. """
   try:
-    data = uc_instance.get_one(id_num)
-    response, status = r_helper.success_response(data)
-  except NoDataException as err:
-    response, status = r_helper.error_response(err.status, details=f"{err}")
-  except Exception as err:
-    response, status = r_helper.unkown_error(details=f"{err}")
-
-  return jsonify(response), status
-
-
-@user_blueprint.route("/<id_num>", methods=["PUT"])
-def update_user(id_num):
-  """ Update a user. """
-  try:
-    uc_instance.update(id_num, request.json)
-    response, status = r_helper.success_response(message="User updated.")
+    if request.method == "GET":
+      data = uc_instance.get_one(id_num)
+      response, status = r_helper.success_response(data)
+    elif request.method == "PUT":
+      uc_instance.update(id_num, request.json)
+      response, status = r_helper.success_response(message="User updated.")
   except (NoDataException, KeyUnmutableException) as err:
     response, status = r_helper.error_response(err.status, details=f"{err}")
-  except Exception as err:
-    response, status = r_helper.unkown_error(details=f"{err}")
-
-  return jsonify(response), status
-
-
-@user_blueprint.route("/", methods=["POST"])
-def create_user():
-  """ Create a user. """
-  try:
-    result = uc_instance.create(request.json)
-    response, status = r_helper.success_response(result, message="New user added.", status=201)
   except Exception as err:
     response, status = r_helper.unkown_error(details=f"{err}")
 
