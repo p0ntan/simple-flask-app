af2cd75e7430bdcfe4877727d0d8936c37c7bb11
p0ntan <pontus.akerberg@hotmail.com>
2024-06-03 11:18:25 +0200

    Add triggers

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,3 +1,5 @@
+-- tables 
+
 DROP TABLE post;
 DROP TABLE topic;
 DROP TABLE user;
@@ -8,6 +10,7 @@ CREATE TABLE "user" (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	username TEXT NOT NULL,
 	password TEXT,
+	role TEST(15) DEFAULT "author",
 	signature TEXT(150),
 	avatar TEXT(150)
 );
@@ -20,8 +23,9 @@ CREATE TABLE topic (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	created_by INTEGER NOT NULL,
 	category INTEGER NOT NULL,
-	deleted TEXT,
-	created TEXT,
+	deleted TIMESTAMP,
+	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	last_edited TIMESTAMP,
 	disabled BOOLEAN,
 	title TEXT NOT NULL,
 	CONSTRAINT topic_user_FK FOREIGN KEY (created_by) REFERENCES "user"(id)
@@ -35,12 +39,27 @@ CREATE TABLE post (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	user_id INTEGER NOT NULL,
 	topic_id INTEGER NOT NULL,
-	deleted TEXT,
-	created TEXT,
-	last_edited TEXT,
+	deleted TIMESTAMP,
+	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	last_edited TIMESTAMP,
 	title TEXT,
 	body TEXT NOT NULL,
 	CONSTRAINT post_user_FK FOREIGN KEY (user_id) REFERENCES "user"(id)
 );
 
 CREATE UNIQUE INDEX post_id_IDX ON post (id);
+
+-- triggers
+
+DROP TRIGGER trg_update_post;
+DROP TRIGGER trg_update_topic;
+
+CREATE TRIGGER trg_update_topic AFTER UPDATE OF title ON topic
+	BEGIN
+		UPDATE topic SET last_edited = CURRENT_TIMESTAMP WHERE id = new.id;
+	END;
+
+CREATE TRIGGER trg_update_post AFTER UPDATE OF body, title ON post
+	BEGIN
+		UPDATE post SET last_edited = CURRENT_TIMESTAMP WHERE id = new.id;
+	END;
--- db/insert.sql
+++ db/insert.sql
@@ -1,1 +1,20 @@
-INSERT INTO user (username) VALUES ("hejhej");
+-- insert example data.
+
+-- users
+
+INSERT INTO user (id, username, role) VALUES (1, "admin", "admin");
+INSERT INTO user (id, username, role) VALUES (3, "moderator", "moderator");
+INSERT INTO user (username) VALUES ("johndoe");
+
+-- topic
+
+INSERT INTO topic (id, created_by, category, title) VALUES (1, 1, 2, "Donald Trump");
+INSERT INTO topic (id, created_by, category, title) VALUES (2, 3, 2, "Joe Biden");
+INSERT INTO topic (id, created_by, category, title) VALUES (3, 1, 4, "Magnus Tolander");
+
+-- post
+
+INSERT INTO post (user_id, topic_id, body) VALUES (1, 2, "Är han världens bästa president?");
+INSERT INTO post (user_id, topic_id, title, body) VALUES (1, 2, "Joe Biden", "Är nog bättre.");
+INSERT INTO post (user_id, topic_id, body) VALUES (3, 4, "Är han rolig eller?");
+INSERT INTO post (user_id, topic_id, body) VALUES (3, 4, "Vad har vi för skit här?");
