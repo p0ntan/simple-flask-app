a4cb5f63542d006aecc1c7595302cba35edd5c40
p0ntan <pontus.akerberg@hotmail.com>
2024-06-04 20:27:52 +0200

    Update usercontroller, move code to own class

--- src/blueprints/api/userblueprint.py
+++ src/blueprints/api/userblueprint.py
@@ -1,47 +1,11 @@
 """
 Blueprint for api route /user
 """
-from flask import Blueprint, jsonify, request
-from flask_cors import cross_origin
+from flask import Blueprint
 from src.controllers.controller_repository import ControllerRepository
-from src.errors.customerrors import NoDataException, KeyUnmutableException
-from src.utils.response_helper import ResponseHelper
 
-r_helper = ResponseHelper()
 user_blueprint = Blueprint('user_blueprint', __name__, url_prefix="/user")
-uc_instance = ControllerRepository().get_user_controller()
+user_controller = ControllerRepository().get_user_controller()
 
-
-@user_blueprint.route("/", methods=["GET", "POST"])
-def index():
-  """ Get all users or create new. """
-  try:
-    # TODO Remove this ? When will this be used?
-    if request.method == "GET":
-      data = uc_instance.get_all()
-      response, status = r_helper.success_response(data)
-    elif request.method == "POST":
-      result = uc_instance.create(request.json)
-      response, status = r_helper.success_response(result, message="New user added.", status=201)
-  except Exception as err:
-    response, status = r_helper.unkown_error(details=f"{err}")
-
-  return jsonify(response), status
-
-
-@user_blueprint.route("/<id_num>", methods=["GET", "PUT"])
-def single_user(id_num):
-  """ Get info from one user. """
-  try:
-    if request.method == "GET":
-      data = uc_instance.get_one(id_num)
-      response, status = r_helper.success_response(data)
-    elif request.method == "PUT":
-      uc_instance.update(id_num, request.json)
-      response, status = r_helper.success_response(message="User updated.")
-  except (NoDataException, KeyUnmutableException) as err:
-    response, status = r_helper.error_response(err.status, details=f"{err}")
-  except Exception as err:
-    response, status = r_helper.unkown_error(details=f"{err}")
-
-  return jsonify(response), status
+user_blueprint.route("/", methods=["POST"])(user_controller.root)
+user_blueprint.route("/<id_num>", methods=["GET", "PUT"])(user_controller.single_user)
--- src/controllers/usercontroller.py
+++ src/controllers/usercontroller.py
@@ -1,11 +1,49 @@
 """
 Usercontroller is for handling all calls to database regarding users.
 """
+from flask import jsonify, request
 from src.controllers.controller import Controller
 from src.utils.dao import DAO
+from src.utils.response_helper import ResponseHelper
+from src.errors.customerrors import NoDataException, KeyUnmutableException
 
+r_helper = ResponseHelper()
 
 class UserController(Controller):
   """ UserController handles all dataaccess for users. """
   def __init__(self, user_dao: DAO):
     super().__init__(dao=user_dao)
+
+  def root(self) -> tuple[dict, int]:
+    """Controller for root route."""
+    try:
+      if request.method == "POST":
+        result = self.create(request.json)
+        response, status = r_helper.success_response(result, message="New user added.", status=201)
+    except Exception as err:
+      response, status = r_helper.unkown_error(details=f"{err}")
+
+    return jsonify(response), status
+
+  def single_user(self, id_num = int) -> tuple[dict, int]:
+    """Controller for single user route
+
+    Parameters:
+      id_num(int):  id for topic
+
+    Returns:
+      response, status
+    """
+    try:
+      if request.method == "GET":
+        data = self.get_one(id_num)
+        response, status = r_helper.success_response(data)
+      elif request.method == "PUT":
+        self.update(id_num, request.json)
+        response, status = r_helper.success_response(message="User updated.")
+    except (NoDataException, KeyUnmutableException) as err:
+      response, status = r_helper.error_response(err.status, details=f"{err}")
+    except Exception as err:
+      response, status = r_helper.unkown_error(details=f"{err}")
+
+    return jsonify(response), status
