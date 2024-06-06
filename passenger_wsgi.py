922f988e9f7f54d91ff6f6e60e177a6f97644bf2
p0ntan <pontus.akerberg@hotmail.com>
2024-06-06 13:56:00 +0200

    Add secret key to applicaiton

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,6 +2,8 @@
 #
 # Test project creating an API and rendering templates on same server
 #
+import re
+import os
 from flask import Flask
 from dotenv import load_dotenv
 from src.blueprints.api.apiblueprint import api_blueprint
@@ -12,6 +14,7 @@ load_dotenv()
 application = Flask(__name__)
 application.register_blueprint(api_blueprint)
 application.register_blueprint(index_blueprint)
+application.secret_key = os.environ.get("SECRET_KEY", re.sub(r"[^a-z\d]", "", os.path.realpath(__file__)))
 
 if __name__ == "__main__":
   print(application.url_map)
