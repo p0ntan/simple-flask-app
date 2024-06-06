bc49ce1d765aa836b0d9bb29f58b184f80bb8839
p0ntan <pontus.akerberg@hotmail.com>
2024-06-06 13:58:14 +0200

    Remove code

--- src/blueprints/index.py
+++ src/blueprints/index.py
@@ -9,9 +9,4 @@ index_blueprint = Blueprint('index_blueprint', __name__, url_prefix="/")
 
 @index_blueprint.route("/")
 def index():
-  url = os.environ.get("API_URL", "")
-  print(url)
-  r = requests.get(f"{url}/topics/2/page/0")
-  data = r.json()
-
-  return render_template("index.html", posts=data["data"]["posts"])
+  return render_template("index.html")
