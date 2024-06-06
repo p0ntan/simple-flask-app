7554108b6647f9d2cfef713953b630056b50ac86
p0ntan <pontus.akerberg@hotmail.com>
2024-06-06 13:56:49 +0200

    Change spelling error

--- src/blueprints/index.py
+++ src/blueprints/index.py
@@ -5,7 +5,7 @@ import os
 import requests
 from flask import Blueprint, render_template
 
-index_blueprint = Blueprint('index_bluepring', __name__, url_prefix="/")
+index_blueprint = Blueprint('index_blueprint', __name__, url_prefix="/")
 
 @index_blueprint.route("/")
 def index():
