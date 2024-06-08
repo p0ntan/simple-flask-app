234c08b76a22f7905318ea11ddba48554929b409
p0ntan <pontus.akerberg@hotmail.com>
2024-06-08 13:58:01 +0200

    Add userdao with a few tests

---
+++ src/utils/userdao.py
@@ -0,0 +1,52 @@
+"""
+UserDAO is used for accessing users.
+"""
+from __future__ import annotations
+from src.models.user import User
+from src.utils.dao import DAO
+from src.utils.print_colors import ColorPrinter
+
+printer = ColorPrinter()
+
+
+class UserDAO(DAO):
+  """ UserDAO for accessing posts. """
+
+  def __init__(self, table_name: str):
+    super().__init__(table_name=table_name)
+
+  def get_user_by_username(self, username: str) -> User | None:
+    """Retrieves a user from the database by their username.
+
+    Args:
+      username (str): The username of the user to retrieve.
+
+    Returns:
+      User: The user object if found
+      None: None if not found.
+
+    Raises:
+      Exception: If an error occurs while retrieving the user.
+    """
+    conn = None
+
+    try:
+      conn, cur = self.get_connection_and_cursor()
+
+      cur.execute("SELECT * FROM user WHERE username = ?", (username, ))
+      column_names = [description[0] for description in cur.description]
+      result = cur.fetchone()
+
+      if result is None:
+        return result
+
+      user_data = dict(zip(column_names, result))
+      user_id: int = user_data.pop("id")
+
+      return User(user_id, user_data)  # type: ignore
+    except Exception as error:
+      printer.print_fail(error)
+      raise error
+    finally:
+      if conn is not None:
+        conn.close()
