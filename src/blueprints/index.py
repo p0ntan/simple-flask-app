2e3653e338f2293d8c22d26a7ab5f0c0646b8d56
p0ntan <pontus.akerberg@hotmail.com>
2024-06-08 08:52:16 +0200

    Update routes, test for rebase

--- src/blueprints/index.py
+++ src/blueprints/index.py
@@ -3,10 +3,39 @@ Index-blueprint, just a file for collecting all API-routes.
 """
 import os
 import requests
-from flask import Blueprint, render_template
+from flask import Blueprint, session, redirect, request, render_template
 
+API_URL = os.environ.get("API_URL")
 index_blueprint = Blueprint('index_blueprint', __name__, url_prefix="/")
+
 
 @index_blueprint.route("/")
 def index():
-  return render_template("index.html")
+  """Index route for page."""
+  user = session.get("user", None)
+
+  return render_template("index.jinja", user=user)
+
+
+@index_blueprint.route("/login", methods=["post"])
+def login():
+  """Login route."""
+  username = request.form['username']
+  
+  try:
+    response = requests.get(API_URL + "/users/" + username, timeout=5)
+    user = response.json()
+    session["user"] = user["data"]
+  except Exception:
+    # Do nothing, just catch error
+    pass
+
+  return redirect("/")
+
+
+@index_blueprint.route("/logout", methods=["post"])
+def logout():
+  """Logout route."""
+  session.pop("user")
+
+  return redirect("/")
