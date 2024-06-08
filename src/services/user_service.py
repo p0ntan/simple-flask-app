b507ef75d7d851ff55e6ef4c472e97808556419e
p0ntan <pontus.akerberg@hotmail.com>
2024-06-08 14:35:42 +0200

    Add service for user

---
+++ src/services/user_service.py
@@ -0,0 +1,42 @@
+"""
+Module for user service, main purpose to handle users in the application.
+
+This will be used by controllers to keep the logic here and not in controller.
+"""
+from src.utils.userdao import UserDAO
+from src.models.user import UserReturnData
+from src.errors.customerrors import NoDataException
+
+
+class UserService:
+  """
+  UserService is used for user handling.
+  """
+  def __init__(self, dao: UserDAO):
+    """Initializes the UserService class.
+
+    Parameters:
+      dao (UserDAO): An instance of the UserDAO class.
+    """
+    self._dao = dao
+
+  def login(self, username: str, password: str) -> UserReturnData:
+    """Login method for user
+
+    Parameters:
+      username (str): The username of the user.
+      password (str): The password of the user.
+
+    Returns:
+      UserReturnData: The user as a dictionary if found.
+    
+    Raises:
+      NoDataException: If no user is found with the given username.
+    """
+    password = password  # TODO add logic for password handling
+    user = self._dao.get_user_by_username(username)
+
+    if user is None:
+      raise NoDataException(f"No user found with username: {username}")
+
+    return user.to_dict()
