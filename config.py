4d56b45378942ba4beb99d26d1d67ddf9d43ed57
p0ntan <pontus.akerberg@hotmail.com>
2024-06-16 09:49:06 +0200

    Add config file for setup and easier database switching for tests

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+
+path = os.path.join(os.path.dirname(__file__), '.env')
+
+load_dotenv(path)
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    DB_PATH = os.getenv("SQLITE_PATH", "")
+    API_URL = os.getenv("API_URL", "")
+    SECRET_KEY = os.getenv("SECRET_KEY", "")
+    JWT_SECRET = os.getenv("JWT_SECRET", "")
+
+class ProductionConfig(Config):
+    DB_PATH = env_var = os.getenv("SQLITE_PATH", "")
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+    DB_PATH = env_var = os.getenv("TEST_SQLITE_PATH", "")
+
+def get_config():
+    """Get config class depending on env-variable. Default production"""
+    env_var = os.getenv("FLASK_ENV")
+
+    if env_var == "development":
+        print("Using development config")
+        return DevelopmentConfig
+    elif env_var == "test":
+        print("Using testing config")
+        return TestingConfig
+
+    print("Using production config")
+    return ProductionConfig
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,25 +2,33 @@
 #
 # Test project creating an API and rendering templates on same server
 #
-import re
-import os
+from config import get_config
 from flask import Flask
-from dotenv import load_dotenv
 from src.blueprints.api.apiblueprint import api_blueprint
 from src.blueprints.index import index_blueprint
 
 from flask_jwt_extended import JWTManager
 
-load_dotenv()
+# application = Flask(__name__)
+# application.secret_key = os.environ.get("SECRET_KEY", re.sub(r"[^a-z\d]", "", os.path.realpath(__file__)))
+# application.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET", "this-is-just-a-not-secret-backup-key")
+# jwt = JWTManager(application)
 
-application = Flask(__name__)
-application.secret_key = os.environ.get("SECRET_KEY", re.sub(r"[^a-z\d]", "", os.path.realpath(__file__)))
-application.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET", "this-is-just-a-not-secret-backup-key")
-jwt = JWTManager(application)
+# application.register_blueprint(api_blueprint)
+# application.register_blueprint(index_blueprint)
 
-application.register_blueprint(api_blueprint)
-application.register_blueprint(index_blueprint)
+def create_app():
+    application = Flask(__name__)
+    application.config.from_object(get_config())
+
+    jwt = JWTManager()
+    jwt.init_app(application)
+
+    application.register_blueprint(api_blueprint)
+    application.register_blueprint(index_blueprint)
+
+    return application
 
 if __name__ == "__main__":
-  print(application.url_map)
-  application.run(debug=True)
+    application = create_app()
+    application.run()
